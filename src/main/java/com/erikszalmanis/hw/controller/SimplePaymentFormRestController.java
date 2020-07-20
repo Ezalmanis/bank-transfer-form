96da7f98e10bb47ac63e691c0207f8e05bdc071a
Ēriks Zalmanis <eriks.zalmanis@gmail.com>
2020-07-20 10:38:44 +0300

    dev branch

--- src/main/java/com/erikszalmanis/hw/controller/SimplePaymentFormRestController.java
+++ src/main/java/com/erikszalmanis/hw/controller/SimplePaymentFormRestController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,7 @@ import java.util.List;
 //TODO, make devmode setting for cors
 
 @RestController
-//@CrossOrigin
+@CrossOrigin
 public class SimplePaymentFormRestController {
 
     private final Logger logger = LoggerFactory.getLogger(SimplePaymentFormRestController.class);
