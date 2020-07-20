99c8f03e01cfeebb5780f31f77076e5bad2e1ae5
Ēriks Zalmanis <eriks.zalmanis@gmail.com>
2020-07-20 10:28:01 +0300

    final touches :(, wish I had more time

--- frontend/src/features/components/payments/restApi/AxiosConfig.js
+++ frontend/src/features/components/payments/restApi/AxiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = '';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-
+//todo, actully make this with dev-mode in mind
 
 export default axios({
     baseURL: "http://localhost:8075",
